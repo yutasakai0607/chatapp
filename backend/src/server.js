e730a9562f904f031975fba8152befb1114a4e6c
yutasakai0607 <yutasakai0607@gmail.com>
2025-11-24 07:30:40 +0000

    フロント追加

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,23 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from "path"
 
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const app = express();
+const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+  app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+  })
+}
 
 app.listen(port, () => {
   // URL表示
